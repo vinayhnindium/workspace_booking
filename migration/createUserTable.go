7476ebda9ae290e26d7afad1fb9a0977292a725e
Madhavi <madhavi.sanwliya@indiumsoft.com>
2022-07-13 18:16:26 +0530

    Minor changes

--- migration/createUserTable.go
+++ migration/createUserTable.go
@@ -10,7 +10,7 @@ func CreateUserTable() {
 
 	r, err := DbPool.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS users (
         id serial PRIMARY KEY,
-        name VARCHAR ( 50 ) UNIQUE NOT NULL,
+        name VARCHAR ( 50 ) NOT NULL,
         email VARCHAR ( 255 ) UNIQUE NOT NULL,
 				encrypted_password text,
 				role_id INTEGER REFERENCES roles (id),
